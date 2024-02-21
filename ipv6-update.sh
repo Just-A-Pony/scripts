8ef319126298f6bcd3697ee533333606ad2e524e
Just_A_Pony <pony@just-a-pony.net>
2024-02-22 00:17:06 +0800

    functionised

--- ipv6-update.sh
+++ ipv6-update.sh
@@ -1,10 +1,18 @@
+set -v
+set -x
 bearer_auth="flmH9Ies7w7Icj_RMmLlDWxDFZyOn679Bv59NntO"
 domain="sakuya-arch.just-a-pony.net"
 zone_identifier="af577fa47883f7f5c9a950086904764e"
-ipv6_address=$(ip --json -6 address show dynamic|jq -r '.[].addr_info[]|select(.preferred_life_time>0).local//empty')
-records=$(curl -s https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records -H "Content-Type: application/json" -H "Authorization: Bearer $bearer_auth")
-dns_identifier=$(echo $records|jq -r ".result|.[]|select(.name==\"$domain\")|.id")
-    data_body="{\"name\":\"$domain\",\"type\":\"AAAA\",\"content\":\"$ipv6_address\",\"comment\":\"$(date -R)\"}"
+get_ipv6_address(){
+    ip --json -6 address show dynamic|jq -r '.[].addr_info[]|select(.preferred_life_time>0).local//empty'
+}
+get_records(){
+    curl -s https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records -H "Content-Type: application/json" -H "Authorization: Bearer $bearer_auth"
+}
+get_dns_identifier(){
+    get_records|jq -r ".result|.[]|select(.name==\"$domain\")|.id"
+}
+data_body="{\"name\":\"$domain\",\"type\":\"AAAA\",\"content\":\"$(get_ipv6_address)\",\"comment\":\"$(date -R)\"}"
 curl -sX PATCH \
-	https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records/$dns_identifier \
+    https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records/"$(get_dns_identifier)" \
 	-H "Content-Type: application/json" -H "Authorization: Bearer $bearer_auth" --data "$data_body"|jq
