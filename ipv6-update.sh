13a7a2b03c95187501a47837718d2016cd842f6b
Just_A_Pony <pony@just-a-pony.net>
2024-02-24 16:08:09 +0800

    modified:   ipv6-update.sh

--- ipv6-update.sh
+++ ipv6-update.sh
@@ -1,5 +1,7 @@
-set -v
-set -x
+#!/bin/bash
+if [ -n "$DEBUG" ];then
+    set -v;set -x
+fi
 bearer_auth="flmH9Ies7w7Icj_RMmLlDWxDFZyOn679Bv59NntO"
 domain="sakuya-arch.just-a-pony.net"
 zone_identifier="af577fa47883f7f5c9a950086904764e"
@@ -13,6 +15,8 @@ get_dns_identifier(){
     get_records|jq -r ".result|.[]|select(.name==\"$domain\")|.id"
 }
 data_body="{\"name\":\"$domain\",\"type\":\"AAAA\",\"content\":\"$(get_ipv6_address)\",\"comment\":\"$(date -R)\"}"
-curl -sX PATCH \
-    https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records/"$(get_dns_identifier)" \
-	-H "Content-Type: application/json" -H "Authorization: Bearer $bearer_auth" --data "$data_body"|jq
+perform(){
+    curl -sX PATCH https://api.cloudflare.com/client/v4/zones/"$1"/dns_records/"$(get_dns_identifier)" -H "Content-Type: application/json" -H "Authorization: Bearer $2" --data "$3"
+}
+result=$(perform $zone_identifier $bearer_auth $data_body)
+echo "$result"|jq
